f6f932656d55a8ea3fd960125c0230143e3db61f
Chandan-Bhat <chandanbhat6@gmail.com>
2022-09-30 14:43:59 +0530

    Updated Clone

---
+++ airbnb_clone/src/data.jsx
@@ -0,0 +1,57 @@
+import img1 from "./imgs/1.webp"
+import img2 from "./imgs/2.webp"
+import img3 from "./imgs/3.webp"
+import img4 from "./imgs/4.webp"
+import img5 from "./imgs/5.webp"
+
+let data = [
+  { "name": "Sidemen, Indonesia",
+  "img": img1,
+  "rate": "4.6",
+  "dist": "4,841 kilometers",
+  "date": "10 - 15 Oct",
+  "price": "$12,991",
+  "time": "night"},
+
+  { "name": "Abisnamel, Indonesia",
+  "img": img2,
+  "rate": "4.88",
+  "dist": "4,951 kilometers",
+  "date": "18 - 23 Apr",
+  "price": "$27,500",
+  "time": "night"},
+
+  { "name": "El Nido, Philippines",
+  "img": img3,
+  "rate": "4.74",
+  "dist": "4,200 kilometers",
+  "date": "1 - 8 Oct",
+  "price": "$11,500",
+  "time": "night"},
+
+  { "name": "Chittorgarh, India",
+  "img": img4,
+  "rate": "New",
+  "dist": "1, 395 kilometers",
+  "date": "7 - 12 Oct",
+  "price": "$3,500",
+  "time": "night"},
+
+  { "name": "Tampaksiring, Indonesia",
+  "img": img5,
+  "rate": "4.88",
+  "dist": "4,800 kilometers",
+  "date": "3 - 8 Dec",
+  "price": "$24,500",
+  "time": "night"},
+
+  { "name": "Sidemen, Indonesia",
+  "img": img1,
+  "rate": "4.6",
+  "dist": "4,841 kilometers",
+  "date": "10 - 15 Oct",
+  "price": "$12,991",
+  "time": "night"}
+];
+
+export default data;
